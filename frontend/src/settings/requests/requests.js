c30fc07e5d9a9e78c0365a5241cd8b240f7da71b
imvizay <vizzaymeena@gmail.com>
2026-07-26 07:50:47 +0530

    feat(frontend): add Tailwind CSS, Axios configuration, and global styling tokens

---
+++ frontend/src/settings/requests/requests.js
@@ -0,0 +1,9 @@
+import { api } from "../axios/setup";
+
+http = {
+    get:(url,config={}) => api.get(url,config),
+    post: (url,config={}) => api.post(url,config={}),
+    put: (url,config={}) => api.put(url,config={}),
+    patch: (url,config={}) => api.patch(url,config={}),
+    delete: (url,config={}) => api.delete(url,config={}),  
+}
